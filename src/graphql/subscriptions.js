c29603466490e345de4286865729632180fbfe2c
lawynnjana <jlawynn@gmail.com>
2020-12-03 20:13:34 -0500

    add user graphql model

---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,45 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateUser = /* GraphQL */ `
+  subscription OnCreateUser {
+    onCreateUser {
+      id
+      username
+      firstName
+      lastName
+      email
+      image
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateUser = /* GraphQL */ `
+  subscription OnUpdateUser {
+    onUpdateUser {
+      id
+      username
+      firstName
+      lastName
+      email
+      image
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteUser = /* GraphQL */ `
+  subscription OnDeleteUser {
+    onDeleteUser {
+      id
+      username
+      firstName
+      lastName
+      email
+      image
+      createdAt
+      updatedAt
+    }
+  }
+`;
