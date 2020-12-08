8c51b28a9c5b5aaebb732e0c2482090719e316fd
lawynnjana <jlawynn@gmail.com>
2020-12-08 16:36:15 -0500

    add get user func

--- amplify/backend/function/EmailReminderService/src/index.js
+++ amplify/backend/function/EmailReminderService/src/index.js
@@ -1,22 +1,42 @@
-var aws = require("aws-sdk");
-var ses = new aws.SES({ region: process.env.AWS_REGION });
+const AWS = require("aws-sdk");
+const ses = new AWS.SES({ region: process.env.AWS_REGION });
 
 exports.handler = async function (event) {
   console.log("EVENT\n" + JSON.stringify(event, null, 2));
 
+  let subject = "No Subject";
+  let body = "";
+
+  const emails = event.Records.reduce((acc, record) => {
+    const message = JSON.parse(record.Sns.Message);
+    const recipients = message.recipients;
+    if (message.subject) {
+      subject = message.subject;
+    }
+    if (message.body) {
+      body = message.body;
+    }
+    const tempEmails = recipients.map((person) => person.email);
+    return acc.concat(tempEmails);
+  }, []);
+
   var params = {
     Destination: {
-      ToAddresses: ["jlawynn@gmail.com"],
+      ToAddresses: emails,
     },
     Message: {
       Body: {
-        Text: { Data: "Test" },
+        Text: { Data: body },
       },
 
-      Subject: { Data: "Test Email" },
+      Subject: { Data: subject },
     },
     Source: process.env.AWS_SES_POKER_APP_EMAIL,
   };
-
-  return ses.sendEmail(params).promise();
+  try {
+    console.log("Sending emails...");
+    await ses.sendEmail(params).promise();
+  } catch (error) {
+    console.log("Error", error);
+  }
 };
--- amplify/backend/function/GameEventProcessor/src/index.js
+++ amplify/backend/function/GameEventProcessor/src/index.js
@@ -1,7 +1,29 @@
 const AWS = require("aws-sdk");
 
+const userTable = process.env.AWS_DDB_USER_TABLE;
+
 AWS.config.update({ region: process.env.AWS_REGION });
+const docClient = new AWS.DynamoDB.DocumentClient();
 const ARN = process.env.AWS_SNS_ARN_POKER_GAME;
+
+async function getUser(userId) {
+  try {
+    const params = {
+      TableName: userTable,
+      Key: {
+        id: userId,
+      },
+    };
+    console.log("Fetchign user...");
+    console.log(params);
+    const user = await docClient.get(params).promise();
+    console.log("User", user);
+    return user;
+  } catch (err) {
+    console.log("Error", err);
+    return err;
+  }
+}
 
 exports.handler = async (event, context) => {
   //eslint-disable-line
@@ -17,17 +39,26 @@ exports.handler = async (event, context) => {
 
   const snsRequests = [];
   const sns = new AWS.SNS();
-  modifiedGames.forEach(({ oldImage, newImage }) => {
+
+  for (const game of modifiedGames) {
+    const { oldImage, newImage } = game;
     // Publish SNS message when game becomes "active"
     if (newImage.status === "ACTIVE" && oldImage.status !== newImage.status) {
+      const user = await getUser(newImage.hostId);
+      console.log("Returned user", user);
       const params = {
-        Message: JSON.stringify(newImage.players),
+        Message: JSON.stringify({
+          subject: "Poker Game Settings",
+          body: "Your game has been created",
+          recipients: [{ email: user.email, name: user.username }],
+        }),
         TopicArn: ARN,
         Subject: "Finalized game settings",
       };
       snsRequests.push(sns.publish(params).promise());
     }
-  });
+  }
+
   try {
     console.log("Request: Publishing messages...");
     await Promise.all(snsRequests);
