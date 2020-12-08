d8da468ce87ec17d9722953372119403dffc9326
lawynnjana <jlawynn@gmail.com>
2020-12-07 22:40:37 -0500

    add lambda for emailing users

--- amplify/backend/function/EmailReminderService/src/index.js
+++ amplify/backend/function/EmailReminderService/src/index.js
@@ -1,14 +1,22 @@
+var aws = require("aws-sdk");
+var ses = new aws.SES({ region: process.env.AWS_REGION });
 
+exports.handler = async function (event) {
+  console.log("EVENT\n" + JSON.stringify(event, null, 2));
 
-exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*"
-    //  }, 
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    return response;
+  var params = {
+    Destination: {
+      ToAddresses: ["jlawynn@gmail.com"],
+    },
+    Message: {
+      Body: {
+        Text: { Data: "Test" },
+      },
+
+      Subject: { Data: "Test Email" },
+    },
+    Source: process.env.AWS_SES_POKER_APP_EMAIL,
+  };
+
+  return ses.sendEmaail(params).promise();
 };
