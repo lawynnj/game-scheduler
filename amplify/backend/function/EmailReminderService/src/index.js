956dd46b1858ca29055ed56c4ade7b5dd4c7ac9c
lawynnjana <jlawynn@gmail.com>
2020-12-08 23:05:50 -0500

    fix a bug that used the same subject/body for other emails

--- amplify/backend/function/EmailReminderService/src/index.js
+++ amplify/backend/function/EmailReminderService/src/index.js
@@ -4,39 +4,35 @@ const ses = new AWS.SES({ region: process.env.AWS_REGION });
 exports.handler = async function (event) {
   console.log("EVENT\n" + JSON.stringify(event, null, 2));
 
-  let subject = "No Subject";
-  let body = "";
-
-  const emails = event.Records.reduce((acc, record) => {
+  let recNum = 0;
+  for (const record of event.Records) {
+    // get recipients, subject and body for email
     const message = JSON.parse(record.Sns.Message);
     const recipients = message.recipients;
-    if (message.subject) {
-      subject = message.subject;
-    }
-    if (message.body) {
-      body = message.body;
-    }
-    const tempEmails = recipients.map((person) => person.email);
-    return acc.concat(tempEmails);
-  }, []);
+    const subject = message.subject ? message.subject : "No Subject";
+    const body = message.body ? message.body : "";
+    const emails = recipients.map((person) => person.email);
 
-  var params = {
-    Destination: {
-      ToAddresses: emails,
-    },
-    Message: {
-      Body: {
-        Text: { Data: body },
+    const params = {
+      Destination: {
+        ToAddresses: emails,
       },
+      Message: {
+        Body: {
+          Text: { Data: body },
+        },
 
-      Subject: { Data: subject },
-    },
-    Source: process.env.AWS_SES_POKER_APP_EMAIL,
-  };
-  try {
-    console.log("Sending emails...");
-    await ses.sendEmail(params).promise();
-  } catch (error) {
-    console.log("Error", error);
+        Subject: { Data: subject },
+      },
+      Source: process.env.AWS_SES_POKER_APP_EMAIL,
+    };
+    try {
+      console.log("Request: Processing record", recNum);
+      await ses.sendEmail(params).promise();
+      console.log("Success: Success");
+      recNum += 1;
+    } catch (error) {
+      console.log("Error", error);
+    }
   }
 };
