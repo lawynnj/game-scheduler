aed62b8af95ee6c3aade493fed6604db7f621576
lawynnjana <jlawynn@gmail.com>
2020-12-10 15:46:43 -0500

    create schedule exp from eventDate

--- amplify/backend/function/EmailReminderCron/src/index.js
+++ amplify/backend/function/EmailReminderCron/src/index.js
@@ -7,97 +7,72 @@
 Amplify Params - DO NOT EDIT */
 const AWS = require("aws-sdk");
 const ARN = process.env.AWS_CWE_ARN_POKER_GAME;
-const CWE_NAME = process.env.CWE_NAME || "poker-scheduler-email-reminder";
-const CWE_DESCRIPTION =
-  process.env.CWE_DESCRIPTION || "Send emails notifications for a game";
-const CWE_RULE_TAG_KEY = process.env.CWE_RULE_TAG_KEY || "Poker scheduler game";
-const CWE_RULE_TAG_VALUE =
-  process.env.CWE_RULE_TAG_VALUE || "The app poker game scheduler";
 const LAMBDA_ARN = process.env.LAMBDA_ARN_POKER_GAME;
-AWS.config.update({ region: process.env.AWS_REGION });
 
+async function initCloudWatchEvents(modifiedGames) {
+  const cwe = new AWS.CloudWatchEvents();
+
+  // const cwRequests = [];
+  for (const game of modifiedGames) {
+    const { oldImage, newImage } = game;
+
+    // Create CloudWatch event when game status is "completed"
+    if (
+      newImage.status === "COMPLETED" &&
+      oldImage.status !== newImage.status
+    ) {
+    }
+
+    try {
+      console.log("Creating rule...");
+
+      const fullDate = new Date(newImage.eventTime);
+      const min = fullDate.getMinutes();
+      const hour = fullDate.getHours();
+      const date = fullDate.getDate();
+      const month = fullDate.getMonth() + 1;
+      const year = fullDate.getFullYear();
+      const schedule = `cron(${min} ${hour} ${date} ${month} ? ${year})`;
+      const ruleParams = {
+        Name: "poker-game-" + newImage.id,
+        Description: "Email notification for game:" + newImage.id,
+        RoleArn: ARN,
+        ScheduleExpression: schedule,
+        State: "ENABLED",
+      };
+
+      await cwe.putRule(ruleParams).promise();
+
+      console.log("Setting targets...");
+      const targetParams = {
+        Rule: "poker-game-" + newImage.id,
+        Targets: [
+          {
+            Arn: LAMBDA_ARN,
+            Id: "1",
+            Input: JSON.stringify({ gameId: newImage.id }),
+          },
+        ],
+      };
+      await cwe.putTargets(targetParams).promise();
+    } catch (error) {
+      console.log("Error", error);
+    }
+  }
+}
 exports.handler = async (event, context) => {
   //eslint-disable-line
   console.log("EVENT\n" + JSON.stringify(event, null, 2));
 
   // map DDB objects to JSON
-  // filter for modified event
-  const modifiedGames = event.Records.map((record) => {
+  const modifiedGames = event.Records.filter(
+    (record) => record.eventName === "MODIFY"
+  ).map((record) => {
     return {
       newImage: AWS.DynamoDB.Converter.unmarshall(record.dynamodb.NewImage),
       oldImage: AWS.DynamoDB.Converter.unmarshall(record.dynamodb.OldImage),
     };
   });
 
-  // const cwRequests = [];
-  const cwe = new AWS.CloudWatchEvents();
-
-  for (const game of modifiedGames) {
-    const { oldImage, newImage } = game;
-
-    // Create CloudWatch event when game status is "completed"
-    // if (newImage.status === "COMPLETED" && oldImage.status !== newImage.status) {
-    // create cloudWatchEvent rule
-
-    try {
-      // cron(Minutes Hours Day-of-month Month Day-of-week Year)
-      const schedule = "cron(0 5 12 12 ? 2020)";
-
-      const ruleParams = {
-        Name: CWE_NAME + "-" + newImage.id /* required */,
-        Description: CWE_DESCRIPTION,
-        RoleArn: ARN,
-        ScheduleExpression: schedule,
-        State: "ENABLED",
-      };
-      console.log("Preparing rule creation...");
-      await cwe.putRule(ruleParams).promise();
-
-      // add permission to lambda
-      const lambda = new AWS.Lambda();
-      var lambdaPermission = {
-        FunctionName: LAMBDA_ARN,
-        StatementId: CWE_NAME + "-" + newImage.id,
-        Action: "lambda:*",
-        Principal: "events.amazonaws.com",
-      };
-
-      console.log("Updating lambda permission...", lambdaPermission);
-      const resLambda = await lambda.addPermission(lambdaPermission).promise();
-      console.log("Perm. response", resLambda);
-
-      const targetParams = {
-        Rule: CWE_NAME + "-" + newImage.id /* required */,
-        Targets: [
-          {
-            Arn: LAMBDA_ARN /* required */,
-            Id: "1" /* required */,
-            Input: JSON.stringify({ gameId: newImage.id }),
-          },
-        ],
-      };
-      console.log("Set targets", targetParams);
-      const res = await cwe.putTargets(targetParams).promise();
-      console.log("Target resoibse", res);
-    } catch (error) {
-      console.log("Error", error);
-    }
-
-    //   // put targets
-
-    //   const ptRes = await cwe.putTargets().promise();
-
-    //   // update the lambda to subscribe to event
-    //   const lambda = AWS.Lambda();
-    //   const permissionParams = {
-    //     FunctionName: "cloudwatch-trigger",
-    //     StatementId: timestamp.toString(),
-    //     Action: "lambda:InvokeFunction",
-    //     Principal: "events.amazonaws.com",
-    //     SourceArn: ruleRes.arn,
-    //   };
-    //   lambda.addPermission(permissionParams);
-  }
-  // }
-  // }
+  await initCloudWatchEvents(modifiedGames);
 };
