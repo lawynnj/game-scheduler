9a5edbf89b167c04acd69b451ca3ab254947b7cf
ljana <jlawynn@gmail.com>
2020-12-10 21:20:58 -0500

    Refactor event process lambda async handling (#5)
    * add filter for modified records
    
    * refactor promises to operate asynchronously
    
    * remove aws-sdk from package.json
    
    * fix typo
    
    * update cloud formation template

--- amplify/backend/function/GameEventProcessor/src/index.js
+++ amplify/backend/function/GameEventProcessor/src/index.js
@@ -10,10 +10,34 @@ const ARN = process.env.AWS_SNS_ARN_POKER_GAME;
 const USER_TABLE = process.env.AWS_DDB_USER_TABLE;
 const SUBJECT = process.env.SNS_SUBJECT || "Finalized game settings";
 const EMAIL_SUBJECT = process.env.EMAIL_SUBJECT || "Poker Game Settings";
-const EMAIL_BODY = process.env.EMAIL_BODY || "Your game has been created";
 
 AWS.config.update({ region: process.env.AWS_REGION });
 const docClient = new AWS.DynamoDB.DocumentClient();
+
+function initSnsEvents(modifiedRecords) {
+  const sns = new AWS.SNS();
+
+  return modifiedRecords.map(async ({ oldImage, newImage }) => {
+    // Publish SNS message when a game becomes "active"
+    if (newImage.status === "ACTIVE" && oldImage.status !== newImage.status) {
+      try {
+        const user = await getUser(newImage.hostId);
+        const params = {
+          Message: JSON.stringify({
+            subject: EMAIL_SUBJECT,
+            body: `Your poker game:${newImage.title} is active!`,
+            recipients: [{ email: user.Item.email, name: user.Item.username }],
+          }),
+          TopicArn: ARN,
+          Subject: SUBJECT,
+        };
+        await sns.publish(params).promise();
+      } catch (error) {
+        console.log("Error", error);
+      }
+    }
+  });
+}
 
 async function getUser(userId) {
   try {
@@ -31,43 +55,23 @@ async function getUser(userId) {
   }
 }
 
-exports.handler = async (event, context) => {
+exports.handler = async (event) => {
   //eslint-disable-line
   console.log("EVENT\n" + JSON.stringify(event, null, 2));
 
   // map DDB objects to JSON
-  const modifiedGames = event.Records.map((record) => {
+  const modifiedRecords = event.Records.filter(
+    (record) => record.eventName === "MODIFY"
+  ).map((record) => {
     return {
       newImage: AWS.DynamoDB.Converter.unmarshall(record.dynamodb.NewImage),
       oldImage: AWS.DynamoDB.Converter.unmarshall(record.dynamodb.OldImage),
     };
   });
 
-  const snsRequests = [];
-  const sns = new AWS.SNS();
-
-  for (const game of modifiedGames) {
-    const { oldImage, newImage } = game;
-
-    // Publish SNS message when game becomes "active"
-    if (newImage.status === "ACTIVE" && oldImage.status !== newImage.status) {
-      const user = await getUser(newImage.hostId);
-      const params = {
-        Message: JSON.stringify({
-          subject: EMAIL_SUBJECT,
-          body: EMAIL_BODY,
-          recipients: [{ email: user.Item.email, name: user.Item.username }],
-        }),
-        TopicArn: ARN,
-        Subject: SUBJECT,
-      };
-      snsRequests.push(sns.publish(params).promise());
-    }
-  }
-
   try {
     console.log("Request: Publishing messages...");
-    await Promise.all(snsRequests);
+    await Promise.all(initSnsEvents(modifiedRecords));
     console.log("Success: Messages published");
   } catch (error) {
     console.log("Error: Messages not published", error);
