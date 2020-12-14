8245b2df28a79c257c28cbf979d9d1aa8cc33f1e
lawynnjana <jlawynn@gmail.com>
2020-12-14 01:55:03 -0500

    add link to homepage in navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,9 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import { AmplifySignOut } from "@aws-amplify/ui-react";
-import { Box } from "@material-ui/core";
+import Box from "@material-ui/core/Box";
+
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
@@ -16,7 +18,7 @@ export default function Header() {
           width="100%"
         >
           <Typography variant="h6" color="inherit">
-            Poker Game Organizer
+            <Link to="/">Poker Game Organizer</Link>
           </Typography>
           <div>
             <AmplifySignOut />
