5724a238a81684a66a4970f37490c75e9a90ae5a
lawynnjana <jlawynn@gmail.com>
2021-01-12 22:16:54 -0500

    refactor to ts

---
+++ src/hooks/useStateWithLocalStorage.ts
@@ -0,0 +1,17 @@
+import React, { Dispatch, SetStateAction, useState, useEffect } from "react";
+
+const useStateWithLocalStorage = (
+  localStorageKey: string
+): [string, Dispatch<SetStateAction<string>>] => {
+  const [value, setValue] = useState(
+    localStorage.getItem(localStorageKey) || ""
+  );
+
+  useEffect(() => {
+    localStorage.setItem(localStorageKey, value);
+  }, [value, localStorageKey]);
+
+  return [value, setValue];
+};
+
+export default useStateWithLocalStorage;
