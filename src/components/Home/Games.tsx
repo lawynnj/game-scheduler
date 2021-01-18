0bbf7cc040da643eb654362402fe9988b7031e53
lawynnjana <jlawynn@gmail.com>
2021-01-18 03:26:19 -0500

    add empty message for game lists

--- src/components/Home/Games.tsx
+++ src/components/Home/Games.tsx
@@ -21,51 +21,66 @@ const Games = (props: GamesProps): JSX.Element => {
     return games.filter((game) => game.status === status);
   };
 
+  const renderDraftGames = (): JSX.Element | JSX.Element[] => {
+    const items = filterGames(GameStatus.PENDING).map((game: GameType) => {
+      const date = new Date(game.createdAt ?? "");
+
+      return (
+        <Box key={game.id} display="flex" flexDirection="row">
+          <RenderItemLink date={date} title={game.title} to={`/edit/${game.id}`} />
+          <Button style={{ width: 150 }} color="primary" onClick={() => onMakeActive({ ...game })}>
+            Make Active
+          </Button>
+          <Button color="secondary" variant="contained" onClick={() => onDelete(game.id)}>
+            Delete
+          </Button>
+        </Box>
+      );
+    });
+
+    return items.length > 0 ? items : <p>No draft games!</p>;
+  };
+
+  const renderActiveGames = (): JSX.Element | JSX.Element[] => {
+    const items = filterGames(GameStatus.ACTIVE).map((game: GameType) => {
+      const date = new Date(game.createdAt ?? "");
+
+      return (
+        <Box display="flex" key={game.id}>
+          <RenderItemLink date={date} title={game.title} to={`/shared/${game.id}`} />
+          <CopyToClipboard text={`${process.env.REACT_APP_DOMAIN}/shared/${game.id}`}>
+            <Button style={{ minWidth: 100 }} size="small">
+              Copy link
+            </Button>
+          </CopyToClipboard>
+          <Button color="secondary" variant="contained" onClick={() => onDelete(game.id)}>
+            Delete
+          </Button>
+        </Box>
+      );
+    });
+
+    return items.length > 0 ? items : <p>No active games!</p>;
+  };
+
+  const renderCompletedGames = (): JSX.Element | JSX.Element[] => {
+    const items = filterGames(GameStatus.ACTIVE).map((game: GameType) => {
+      return <RenderItemLink key={game.id} title={game.title} to={`/shared/${game.id}`} />;
+    });
+
+    return items.length > 0 ? items : <p>No completed games!</p>;
+  };
+
   return (
     <>
       <p>Draft Games:</p>
-      <List>
-        {filterGames(GameStatus.PENDING).map((game: GameType) => {
-          const date = new Date(game.createdAt ?? "");
-
-          return (
-            <Box key={game.id} display="flex" flexDirection="row">
-              <RenderItemLink date={date} title={game.title} to={`/edit/${game.id}`} />
-              <Button style={{ width: 150 }} color="primary" onClick={() => onMakeActive({ ...game })}>
-                Make Active
-              </Button>
-              <Button color="secondary" variant="contained" onClick={() => onDelete(game.id)}>
-                Delete
-              </Button>
-            </Box>
-          );
-        })}
-      </List>
+      <List>{renderDraftGames()}</List>
       <Divider style={{ height: 1, marginTop: 10 }} />
       <p>Active Games:</p>
-      <List>
-        {filterGames(GameStatus.ACTIVE).map((game: GameType) => {
-          const date = new Date(game.createdAt ?? "");
-
-          return (
-            <Box display="flex" key={game.id}>
-              <RenderItemLink date={date} title={game.title} to={`/shared/${game.id}`} />
-              <CopyToClipboard text={`${process.env.REACT_APP_DOMAIN}/shared/${game.id}`}>
-                <Button style={{ minWidth: 100 }} size="small">
-                  Copy link
-                </Button>
-              </CopyToClipboard>
-            </Box>
-          );
-        })}
-      </List>
+      <List>{renderActiveGames()}</List>
       <Divider style={{ height: 1, marginTop: 10 }} />
       <p>Completed Games:</p>
-      <List>
-        {filterGames(GameStatus.ACTIVE).map((game: GameType) => {
-          return <RenderItemLink key={game.id} title={game.title} to={`/shared/${game.id}`} />;
-        })}
-      </List>
+      <List>{renderCompletedGames()}</List>
     </>
   );
 };
