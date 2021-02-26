7ea6b09cf0eb9f47f0089eb8fc1fc34137c4b5e9
ljana <jlawynn@gmail.com>
2021-02-25 20:36:16 -0500

    remove host from Game schema (#34)

--- src/graphql/queries.ts
+++ src/graphql/queries.ts
@@ -21,7 +21,11 @@ export const getUser = /* GraphQL */ `
   }
 `;
 export const listUsers = /* GraphQL */ `
-  query ListUsers($filter: ModelUserFilterInput, $limit: Int, $nextToken: String) {
+  query ListUsers(
+    $filter: ModelUserFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
     listUsers(filter: $filter, limit: $limit, nextToken: $nextToken) {
       items {
         id
@@ -72,7 +76,11 @@ export const getGame = /* GraphQL */ `
   }
 `;
 export const listGames = /* GraphQL */ `
-  query ListGames($filter: ModelGameFilterInput, $limit: Int, $nextToken: String) {
+  query ListGames(
+    $filter: ModelGameFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
     listGames(filter: $filter, limit: $limit, nextToken: $nextToken) {
       items {
         id
